ab74371faf2e9cc74be6c46246f462bd97ff92fc
Latz <latz@elektroelch.de>
2026-05-01 19:07:47 +0200

    fix: remove radio indicator from mode cards

--- src/schedule/components/ScheduleTypePicker.jsx
+++ src/schedule/components/ScheduleTypePicker.jsx
@@ -42,13 +42,6 @@ export default function ScheduleTypePicker({ value, onChange }) {
                   onClick={() => onChange(mode.value)}
                   type="button"
                 >
-                  <span className="linkdigest-mode-card__check">
-                    {active && (
-                      <svg width="8" height="8" viewBox="0 0 8 8" fill="none">
-                        <circle cx="4" cy="4" r="3" fill="#fff" />
-                      </svg>
-                    )}
-                  </span>
                   <div className="linkdigest-mode-card__title">{mode.label}</div>
                   <div className="linkdigest-mode-card__desc">{mode.desc}</div>
                 </button>
